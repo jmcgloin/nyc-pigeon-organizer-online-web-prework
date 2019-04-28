b7ae7ae73a361745d670dae0cb09c5ff2ecd3894
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-28 23:42:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  output_data = {}
+  keys = data.keys
+  values = []
+  keys.each do
+    |descriptions|
+    values.push 
+  binding.pry
 end
+
+
+def clip()
+if !output_data.key?(name) then output_data[name] = {} end
+      if !output_data[name].key?(key) then output_data[name][key] = [] end
+      if !output_data[name][key].include?(value) then output_data[name][key].push(value) end
+      end
