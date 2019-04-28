69a0c939cb7fc2bd0700f739e6fe51a31bc7c3d0
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-28 23:48:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,10 @@ def nyc_pigeon_organizer(data)
   output_data = {}
   # pige_names - the names of the pigeons, will be the top level keys
   # pige_keys - the categories, will be second level
+  pige_keys = data.keys
   # pige_values - the values of the categories
+  
+  
   
   binding.pry
   
