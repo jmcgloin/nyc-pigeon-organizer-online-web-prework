d641323ed3df69bee22fc34a43c94ea22aa853c8
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-29 01:35:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -42,21 +42,21 @@ def nyc_pigeon_organizer(data)
   
       
   
-  binding.pry
+  # binding.pry
 end
 
-data = {:color=>
-  {:purple=>["Theo", "Peter Jr.", "Lucky"],
-   :grey=>["Theo", "Peter Jr.", "Ms. K"],
-   :white=>["Queenie", "Andrew", "Ms. K", "Alex"],
-   :brown=>["Queenie", "Alex"]},
- :gender=>
-  {:male=>["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-   :female=>["Queenie", "Ms. K"]},
- :lives=>
-  {"Subway"=>["Theo", "Queenie"],
-   "Central Park"=>["Alex", "Ms. K", "Lucky"],
-   "Library"=>["Peter Jr."],
-   "City Hall"=>["Andrew"]}}
+# data = {:color=>
+#   {:purple=>["Theo", "Peter Jr.", "Lucky"],
+#   :grey=>["Theo", "Peter Jr.", "Ms. K"],
+#   :white=>["Queenie", "Andrew", "Ms. K", "Alex"],
+#   :brown=>["Queenie", "Alex"]},
+# :gender=>
+#   {:male=>["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+#   :female=>["Queenie", "Ms. K"]},
+# :lives=>
+#   {"Subway"=>["Theo", "Queenie"],
+#   "Central Park"=>["Alex", "Ms. K", "Lucky"],
+#   "Library"=>["Peter Jr."],
+#   "City Hall"=>["Andrew"]}}
    
-nyc_pigeon_organizer(data)
+# nyc_pigeon_organizer(data)
