69e915b0c281937a519376637849c22c02a36e4e
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-29 01:46:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -33,7 +33,7 @@ def nyc_pigeon_organizer(data)
         |name|
         if data[key][val].include?(name)
           if !output_data[name].key?(key) then output_data[name][key] = [] end
-          if name == "Alex" && key == :lives && val = "City Hall" then binding.pry end
+          # if name == "Alex" && key == :lives && val = "City Hall" then binding.pry end
           if !output_data[name][key].include?(val) then output_data[name][key].push(val) end
         end
       end
