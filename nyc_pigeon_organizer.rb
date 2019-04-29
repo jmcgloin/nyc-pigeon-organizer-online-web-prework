acd7c27219da8a8cbed5e391208f7405313b9497
Jason McGloin <jason.mcgloin@gmail.com>
2019-04-29 00:37:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -31,7 +31,7 @@ def nyc_pigeon_organizer(data)
         if !output_data[name][key].include?(value) && data[key].key?(value)
           if data[key][value].include?(name) then output_data[name][key].push(value) end
         end
-        if value == "Central Park" then binding.pry end
+        if value == "Library" then binding.pry end
       end
     end
   end
